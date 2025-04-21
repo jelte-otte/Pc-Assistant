29107aa40ac81ed10a6e15102cfc429e2850d02c
remco.zuidweg <remco.zuidweg@profects.com>
2025-04-21 15:32:57 +0200

    added minecraft support

--- app_functions/open_app.py
+++ app_functions/open_app.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import json
 from fuzzywuzzy import fuzz
+import win32com.client
 
 def load_apps():
     path = os.path.join("apps", "installed_apps.json")
@@ -41,6 +42,11 @@ def open_app(path_or_cmd):
                 return True
     return False
 
+def resolve_lnk(lnk_path):
+    shell = win32com.client.Dispatch("WScript.Shell")
+    shortcut = shell.CreateShortcut(lnk_path)
+    return shortcut.Targetpath
+
 def open_requested_app():
     apps = load_apps()
     if not apps:
@@ -50,6 +56,13 @@ def open_requested_app():
     best_match = find_best_match(apps, user_input)
 
     if best_match:
+        if best_match == "Prism Launcher":
+            version = input("welke versie?")
+            command = [
+                resolve_lnk(apps[best_match]), 
+                "-l", version,
+            ]
+            subprocess.run(command)
         print(f"Beste overeenkomst: {best_match}")
         success = open_app(apps[best_match])
         if not success:
