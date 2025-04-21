6dad9bf0e29644bfafd74d7dadc2c9f0aa3f3df1
jelte <Jelte@van-de-velde.nl>
2025-04-21 17:07:52 +0200

    moved files

--- call_assistant.py
+++ call_assistant.py
@@ -1,9 +1,9 @@
 from app_functions.get_apps import get_installed_apps
 from app_functions.open_app import open_requested_app
-from messaging_functions.send_whatsapp_message import send_whatsapp_message
-from messaging_functions.get_contacts import get_contacts
-from messaging_functions.send_email import send_email
-from messaging_functions.get_phone_number import get_contact_phone_number
+from messaging_functions.whatsapp.send_whatsapp_message import send_whatsapp_message
+from messaging_functions.whatsapp.get_contacts import get_contacts
+from messaging_functions.mail.send_email import send_email
+from messaging_functions.whatsapp.get_phone_number import get_contact_phone_number
 
 
 get_installed_apps()
