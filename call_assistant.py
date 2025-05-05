3dd145dffcbb1c93e8527a750440db179d091298
jelte <Jelte@van-de-velde.nl>
2025-05-05 11:36:20 +0200

    resolved merge conflicts

--- call_assistant.py
+++ call_assistant.py
@@ -2,11 +2,10 @@ from app_functions.get_apps import get_installed_apps
 from app_functions.get_games import get_installed_games
 from app_functions.open_app import open_requested_app
 from messaging_functions.whatsapp.send_whatsapp_message import send_whatsapp_message
-from app_functions.merge_installed_apps_with_steam_ids import merge_installed_apps_with_steam_ids
 from messaging_functions.whatsapp.get_contacts import get_contacts
 from messaging_functions.mail.send_email import send_email
 from messaging_functions.whatsapp.get_phone_number import get_contact_phone_number
-from messaging_functions.mail.send_email import send_email
+from app_functions.merge_installed_apps_with_steam_ids import merge_installed_apps_with_steam_ids
 
 
 get_installed_apps()
