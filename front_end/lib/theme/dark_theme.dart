6b5a4e16e8ff16c07d489ff96f2457611bebb505
jelte <Jelte@van-de-velde.nl>
2025-05-17 14:44:37 +0200

    added front end

---
+++ front_end/lib/theme/dark_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Colors.grey[900],
+  hintColor: Colors.blueAccent,
+  // Define additional dark theme properties here
+);
---
+++ front_end/lib/theme/light_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: Colors.blue,
+  hintColor: Colors.blueAccent,
+  // Define additional light theme properties here
+);
