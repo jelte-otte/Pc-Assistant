69408da8e212ca0af9c0734c7a296bf6c7862077
jelte <Jelte@van-de-velde.nl>
2025-05-24 12:52:54 +0200

    added python bridge

---
+++ front_end/lib/python_bridge/python_bridge.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
+class PythonBridge {
+  Process? _process;
+  IOSink? _stdin;
+  late Stream<String> _stdoutStream;
+
+  Future<void> start() async {
+    _process = await Process.start(
+      'python',
+      ['check_for_dangerous_apps.py'],
+      workingDirectory: 'D:/Apps/Pc-Assistant',
+      runInShell: true,
+    );
+    _stdin = _process!.stdin;
+
+    _stdoutStream = _process!.stdout
+        .transform(utf8.decoder)
+        .transform(const LineSplitter());
+
+    _stdoutStream.listen((line) {
+      log("Backend zei: $line");
+    });
+    _process!.stderr
+        .transform(utf8.decoder)
+        .transform(const LineSplitter())
+        .listen((line) {
+          log("Python stderr: $line");
+        });
+
+    _process!.exitCode.then((code) {
+      log('Python script exited with code $code');
+    });
+  }
+
+  void send(String text) {
+    _stdin?.writeln(text);
+  }
+
+  void stop() {
+    _process?.kill();
+  }
+}
